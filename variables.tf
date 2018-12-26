4244b5fc80d8ee0a24219e6611b42e56d46d7891
James Woolfenden <james.woolfenden@gmail.com>
2018-12-26 20:46:15 +0000

    with docs

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "name" {
-    description="We are following tech-resource-topic after hashicorp."
+  description = "We are following tech-resource-topic after hashicorp."
 }
 
 variable "description" {
-    description="Put more effort into to descibing what your repo is about."
+  description = "Put more effort into to descibing what your repo is about."
 }
 
 variable "has_wiki" {
-    description="This enables a wiki on your github repo. Why not."
+  description = "This enables a wiki on your github repo. Why not."
 }
 
 variable "private" {
-  description="Unless you have paid for private repos this ones going to be false."
+  description = "Unless you have paid for private repos this ones going to be false."
 }
